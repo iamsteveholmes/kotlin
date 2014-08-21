d85416044aa1415a6fcef32d040c8372b3bda979
Denis Zharkov <bintree@eblans.org>
2014-08-21 08:03:31 +0400

    Extracted inline markers class/methods names

--- compiler/backend/src/org/jetbrains/jet/codegen/inline/InlineCodegenUtil.java
+++ compiler/backend/src/org/jetbrains/jet/codegen/inline/InlineCodegenUtil.java
@@ -69,6 +69,9 @@ public class InlineCodegenUtil {
     public static final String NON_LOCAL_RETURN = "$$$$$NON_LOCAL_RETURN$$$$$";
 
     public static final String ROOT_LABEL = "$$$$$ROOT$$$$$";
+    public static final String INLINE_MARKER_CLASS_NAME = "kotlin/jvm/internal/InlineMarker";
+    public static final String INLINE_MARKER_BEFORE_METHOD_NAME = "beforeInlineCall";
+    public static final String INLINE_MARKER_AFTER_METHOD_NAME = "afterInlineCall";
 
     @Nullable
     public static MethodNode getMethodNode(
@@ -316,8 +319,8 @@ public class InlineCodegenUtil {
             @NotNull InstructionAdapter v,
             boolean isStartNotEnd
     ) {
-        v.visitMethodInsn(Opcodes.INVOKESTATIC, "kotlin/jvm/internal/InlineMarker",
-                          (isStartNotEnd ? "before" : "after") + "InlineCall",
+        v.visitMethodInsn(Opcodes.INVOKESTATIC, INLINE_MARKER_CLASS_NAME,
+                          (isStartNotEnd ? INLINE_MARKER_BEFORE_METHOD_NAME : INLINE_MARKER_AFTER_METHOD_NAME),
                           "()V", false);
 
     }
